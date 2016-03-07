2937bda239caadc52ff0b457542dbdb5fc241dfd
Damian Oscik <oscik@mir.de>
2016-03-07 10:47:08 +0100

    django suit adjustment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,24 @@
 module.exports = function (grunt) {
     "use strict";
 
-    var BoostrapCK4Skin;
+    var KristallCK4Skin;
 
-    var resourcesPath = 'skins/bootstrapck/';
+    var resourcesPath = 'skins/kristallck/';
 
-    BoostrapCK4Skin = {
-        'scss':     ['skins/bootstrapck/scss/**/*.scss']
+    KristallCK4Skin = {
+        'scss':     ['skins/kristallck/scss/**/*.scss']
     };
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         watch: {
-            BoostrapCK4SkinScss: {
-                files: BoostrapCK4Skin.scss,
+            KristallCK4SkinScss: {
+                files: KristallCK4Skin.scss,
                 tasks: ['sass', 'cssmin']
             },
             livereload: {
-                files: ['skins/bootstrapck/*.css'],
+                files: ['skins/kristallck/*.css'],
                 options: {
                     livereload: true
                 }
@@ -26,65 +26,65 @@ module.exports = function (grunt) {
         },
 
         sass: {
-            BoostrapCK4Skin: {
+            KristallCK4Skin: {
                 options: {
                     style: 'compressed'
                 },
                 files: [{
-                    'skins/bootstrapck/.temp/css/editor.css': 'skins/bootstrapck/scss/components/editor.scss',
-                    'skins/bootstrapck/.temp/css/dialog.css': 'skins/bootstrapck/scss/dialog/dialog.scss',
-                    'skins/bootstrapck/.temp/css/editor_ie.css': 'skins/bootstrapck/scss/browser-specific/ie/editor_ie.scss',
-                    'skins/bootstrapck/.temp/css/dialog_ie.css': 'skins/bootstrapck/scss/browser-specific/ie/dialog_ie.scss',
-                    'skins/bootstrapck/.temp/css/editor_ie8.css': 'skins/bootstrapck/scss/browser-specific/ie8/editor_ie8.scss',
-                    'skins/bootstrapck/.temp/css/dialog_ie8.css': 'skins/bootstrapck/scss/browser-specific/ie8/dialog_ie8.scss',
-                    'skins/bootstrapck/.temp/css/editor_ie7.css': 'skins/bootstrapck/scss/browser-specific/ie7/editor_ie7.scss',
-                    'skins/bootstrapck/.temp/css/dialog_ie7.css': 'skins/bootstrapck/scss/browser-specific/ie7/dialog_ie7.scss',
-                    'skins/bootstrapck/.temp/css/editor_iequirks.css': 'skins/bootstrapck/scss/browser-specific/iequirks/editor_iequirks.scss',
-                    'skins/bootstrapck/.temp/css/dialog_iequirks.css': 'skins/bootstrapck/scss/browser-specific/iequirks/dialog_iequirks.scss',
-                    'skins/bootstrapck/.temp/css/editor_gecko.css': 'skins/bootstrapck/scss/browser-specific/gecko/editor_gecko.scss',
-                    'skins/bootstrapck/.temp/css/dialog_opera.css': 'skins/bootstrapck/scss/browser-specific/opera/dialog_opera.scss'
+                    'skins/kristallck/.temp/css/editor.css': 'skins/kristallck/scss/components/editor.scss',
+                    'skins/kristallck/.temp/css/dialog.css': 'skins/kristallck/scss/dialog/dialog.scss',
+                    'skins/kristallck/.temp/css/editor_ie.css': 'skins/kristallck/scss/browser-specific/ie/editor_ie.scss',
+                    'skins/kristallck/.temp/css/dialog_ie.css': 'skins/kristallck/scss/browser-specific/ie/dialog_ie.scss',
+                    'skins/kristallck/.temp/css/editor_ie8.css': 'skins/kristallck/scss/browser-specific/ie8/editor_ie8.scss',
+                    'skins/kristallck/.temp/css/dialog_ie8.css': 'skins/kristallck/scss/browser-specific/ie8/dialog_ie8.scss',
+                    'skins/kristallck/.temp/css/editor_ie7.css': 'skins/kristallck/scss/browser-specific/ie7/editor_ie7.scss',
+                    'skins/kristallck/.temp/css/dialog_ie7.css': 'skins/kristallck/scss/browser-specific/ie7/dialog_ie7.scss',
+                    'skins/kristallck/.temp/css/editor_iequirks.css': 'skins/kristallck/scss/browser-specific/iequirks/editor_iequirks.scss',
+                    'skins/kristallck/.temp/css/dialog_iequirks.css': 'skins/kristallck/scss/browser-specific/iequirks/dialog_iequirks.scss',
+                    'skins/kristallck/.temp/css/editor_gecko.css': 'skins/kristallck/scss/browser-specific/gecko/editor_gecko.scss',
+                    'skins/kristallck/.temp/css/dialog_opera.css': 'skins/kristallck/scss/browser-specific/opera/dialog_opera.scss'
                 }]
             }
         },
 
         cssmin: {
-            BoostrapCK4Skin: {
+            KristallCK4Skin: {
                 files: {
-                    'skins/bootstrapck/editor.css': [
-                        'skins/bootstrapck/.temp/css/editor.css'
+                    'skins/kristallck/editor.css': [
+                        'skins/kristallck/.temp/css/editor.css'
                     ],
-                    'skins/bootstrapck/dialog.css': [
-                        'skins/bootstrapck/.temp/css/dialog.css'
+                    'skins/kristallck/dialog.css': [
+                        'skins/kristallck/.temp/css/dialog.css'
                     ],
-                    'skins/bootstrapck/editor_ie.css': [
-                        'skins/bootstrapck/.temp/css/editor_ie.css'
+                    'skins/kristallck/editor_ie.css': [
+                        'skins/kristallck/.temp/css/editor_ie.css'
                     ],
-                    'skins/bootstrapck/dialog_ie.css': [
-                        'skins/bootstrapck/.temp/css/dialog_ie.css'
+                    'skins/kristallck/dialog_ie.css': [
+                        'skins/kristallck/.temp/css/dialog_ie.css'
                     ],
-                    'skins/bootstrapck/editor_ie8.css': [
-                        'skins/bootstrapck/.temp/css/editor_ie8.css'
+                    'skins/kristallck/editor_ie8.css': [
+                        'skins/kristallck/.temp/css/editor_ie8.css'
                     ],
-                    'skins/bootstrapck/dialog_ie8.css': [
-                        'skins/bootstrapck/.temp/css/dialog_ie8.css'
+                    'skins/kristallck/dialog_ie8.css': [
+                        'skins/kristallck/.temp/css/dialog_ie8.css'
                     ],
-                    'skins/bootstrapck/editor_ie7.css': [
-                        'skins/bootstrapck/.temp/css/editor_ie7.css'
+                    'skins/kristallck/editor_ie7.css': [
+                        'skins/kristallck/.temp/css/editor_ie7.css'
                     ],
-                    'skins/bootstrapck/dialog_ie7.css': [
-                        'skins/bootstrapck/.temp/css/dialog_ie7.css'
+                    'skins/kristallck/dialog_ie7.css': [
+                        'skins/kristallck/.temp/css/dialog_ie7.css'
                     ],
-                    'skins/bootstrapck/editor_iequirks.css': [
-                        'skins/bootstrapck/.temp/css/editor_iequirks.css'
+                    'skins/kristallck/editor_iequirks.css': [
+                        'skins/kristallck/.temp/css/editor_iequirks.css'
                     ],
-                    'skins/bootstrapck/dialog_iequirks.css': [
-                        'skins/bootstrapck/.temp/css/dialog_iequirks.css'
+                    'skins/kristallck/dialog_iequirks.css': [
+                        'skins/kristallck/.temp/css/dialog_iequirks.css'
                     ],
-                    'skins/bootstrapck/editor_gecko.css': [
-                        'skins/bootstrapck/.temp/css/editor_gecko.css'
+                    'skins/kristallck/editor_gecko.css': [
+                        'skins/kristallck/.temp/css/editor_gecko.css'
                     ],
-                    'skins/bootstrapck/dialog_opera.css': [
-                        'skins/bootstrapck/.temp/css/dialog_opera.css'
+                    'skins/kristallck/dialog_opera.css': [
+                        'skins/kristallck/.temp/css/dialog_opera.css'
                     ]
                 }
             }
